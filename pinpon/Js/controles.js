8f3dc55ed9f9cf84133b29498bef91e875923815
luigi-rodriguez <57381035+luigi-rodriguez@users.noreply.github.com>
2019-12-16 19:04:40 -0600

    commit 1
    Video Juego de Ping Pong Con Inteligencia Artificial

---
+++ pinpon/Js/controles.js
@@ -0,0 +1,49 @@
+var Direccion ={QUIETO:0, ARRIBA: 1, ABAJO:2};
+function Controles(){
+  this.ARRIBA=false;
+  this.ABAJO=false;
+  this.W=false;
+  this.S=false;
+var self=this;
+document.body.onkeydown=function(e){
+  switch (e.keyCode) {
+    case 38: //Arriba
+      e.preventDefault();
+      self.ARRIBA=true;
+      break;
+      case 40:
+        e.preventDefault();
+        self.ABAJO=true;
+      break;
+      case 87: //Arriba
+        e.preventDefault();
+        self.W=true;
+      break;
+      case 83: //Arriba
+        e.preventDefault();
+        self.S=true;
+      break;
+  }
+};
+document.body.onkeyup=function(e){
+  switch(e.keyCode){
+    case 38: //Arriba
+      e.preventDefault();
+      self.ARRIBA=false;
+      break;
+      case 40:
+        e.preventDefault();
+        self.ABAJO=false;
+      break;
+      case 87: //Arriba
+        e.preventDefault();
+        self.W=false;
+      break;
+      case 83: //Arriba
+        e.preventDefault();
+        self.S=false;
+      break;
+
+  }
+};
+}
